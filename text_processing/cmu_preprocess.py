41a69c515286f0a719891fa228e8c687e41de59b
Robert Young <robert.young.ie@gmail.com>
2020-03-05 17:43:29 +0000

    refactor: Update to readability and modularity of code

--- text_processing/cmu_preprocess.py
+++ text_processing/cmu_preprocess.py
@@ -1,26 +1,38 @@
-
 from string import digits
 
-cmu_dict = {}
-cmu_dict_file = open("C:/Users/robert/Documents/zeeko_nlp/input_files/cmudict-0.7b", "r")
-cmu_phones = []
-cmu_dict2 = {}
 
-# creating dictionary for cmu dict
-# create one dictionary with phones as key and word as values
-# create a second dictionary with word as key and phone as value
-for line in cmu_dict_file:
-    line = line.strip()
+def process_lines(input_line):
+
+    input_line = input_line.strip()
     remove_digits = str.maketrans('', '', digits)
-    line = line.translate(remove_digits)
-    start = line[:1]
-    # Make sure that we start by a letter from the alphabet, lower or upper case
-    # ord("a") = 97 ||  ord("A") = 65 || ord("z") = 122 || ord(Z) = 90
-    if (ord(start) > 64 and ord(start) < 91) or (ord(start) > 64 and ord(start) < 123):
-        word, phones = line.split("  ")
-        word = word.strip("()")
-        cmu_dict2[word.lower()] = phones
-        cmu_dict[phones] = word.lower()
-        cmu_phones.append(phones)
+    input_line = input_line.translate(remove_digits)
+    return input_line
 
-print(len(cmu_dict))
+
+with open("C:/Users/robert/Documents/zeeko_nlp/input_files/cmudict-0.7b", "r") as cmu_dict_file:
+
+    cmu_dict = {}
+    cmu_phones = []
+    cmu_dict2 = {}
+
+    # creating dictionary for cmu dict
+    # create one dictionary with phones as key and word as values
+    # create a second dictionary with word as key and phone as value
+    for line in cmu_dict_file:
+
+        line_processed = process_lines(line)
+
+        # Make sure that we start by a letter from the alphabet, lower or upper case
+        if line_processed and line_processed[0].isalpha():
+            word, phonemes = line_processed.split("  ")
+            word = word.strip("()")
+
+
+
+
+            # cmu_dict2[word.lower()] = phones
+            # cmu_dict[phones] = word.lower()
+            # cmu_phones.append(phones)
+
+
+
