ea6f29d7ff6d15a38eae4e7a317326c68b42788c
Robert Young <robert.young.ie@gmail.com>
2020-03-13 13:34:49 +0000

    docs: Added docstring for function.

--- text_processing/valid_word_process.py
+++ text_processing/valid_word_process.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 
 def pickle_valid_words():
-
+    """
+    Function which takes the valid word list (list of words that are valid but that are not in the enchant dictionary)
+    and creates a set which is serialised as a pickle file for use in processing the text item.
+    :return: None
+    """
     valid_words_set = set()
     data_folder = Path("C:/Users/robert/Documents/zeeko_nlp/spelling_mistakes/")
     valid_words_file_input = data_folder / 'valid_words.txt'
@@ -12,7 +16,6 @@ def pickle_valid_words():
     with open(valid_words_file_input, 'r') as valid_words:
         lines = valid_words.read().splitlines()
         for word in lines:
-            print(word)
             valid_words_set.add(word)
 
     with open(valid_words_set_output, 'wb') as output_file:
