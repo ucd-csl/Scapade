3d780b679070ea5e80e2552ee2d5e7b8eeabc581
Robert Young <robert.young.ie@gmail.com>
2020-03-13 11:42:04 +0000

    bug: Removed newline being added as a word

--- text_processing/valid_word_process.py
+++ text_processing/valid_word_process.py
@@ -6,13 +6,16 @@ def pickle_valid_words():
 
     valid_words_set = set()
     data_folder = Path("C:/Users/robert/Documents/zeeko_nlp/spelling_mistakes/")
-    valid_words_file = data_folder / 'valid_words.txt'
+    valid_words_file_input = data_folder / 'valid_words.txt'
+    valid_words_set_output = data_folder / 'valid_words_set.txt'
 
-    with open(valid_words_file, 'r') as valid_words:
-        for word in valid_words:
+    with open(valid_words_file_input, 'r') as valid_words:
+        lines = valid_words.read().splitlines()
+        for word in lines:
+            print(word)
             valid_words_set.add(word)
 
-    with open('valid_words_set.txt', 'wb') as output_file:
+    with open(valid_words_set_output, 'wb') as output_file:
         pickle.dump(valid_words_set, output_file)
 
 
