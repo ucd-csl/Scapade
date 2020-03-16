eaafea5cc0b1e53f31ff3cb1b146a6a9d8ddfd9b
Robert Young <robert.young.ie@gmail.com>
2020-03-16 16:11:02 +0000

    feat: Edit distance candidate creation

--- cmu_phoneme_comparison.py
+++ cmu_phoneme_comparison.py
@@ -1,53 +1,67 @@
-import pickle
 from phoneme_edit_distance import phoneme_edit_distance
+import pandas as pd
+from pathlib import Path
+import timeit
+import re
+
+data_folder = Path("C:/Users/robert/Documents/zeeko_nlp/input_files/")
+similarities = data_folder / 'acoustic_similarity.csv'
+similarities = pd.read_csv(similarities, index_col=0)
+
+cmu_phonemes = data_folder / 'cmu_processed.csv'
+cmu_phonemes = pd.read_csv(cmu_phonemes, encoding = "ISO-8859-1")
+
+phonemes = []
+cmu_valid_phonemes = []
+
+for column in similarities.columns:
+    phonemes.append(column)
+
+valid_phonemes = (cmu_phonemes.iloc[:,1])
+valid_phonemes = valid_phonemes.tolist()
+
+#  https://norvig.com/spell-correct.html
+
+def candidates(word):
+    "Generate possible spelling corrections for word."
+    return (known(edits1(word)) or known(edits2(word)))
+
+def known(words):
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in valid_phonemes)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters = phonemes
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [re.sub(' +', ' ',(L + " " + R[1:]).strip()) for L, R in splits if R]
+    transposes = [re.sub(' +', ' ',(L + " " + R[1] + " " + R[0] + " " + R[2:]).strip()) for L, R in splits if len(R)>1]
+    replaces = [re.sub(' +', ' ',(L + " " + c + " " + R[1:]).strip()) for L, R in splits if R for c in letters]
+    inserts = [re.sub(' +', ' ',(L + " " + c + " " + R).replace("  ", " ").strip()) for L, R in splits for c in letters]
+    return set([word] + deletes + transposes + replaces + inserts)
+
+def edits2(word):
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+start = timeit.default_timer()
+
+word_candidates = (candidates('N OW N'))
+scores = {}
+
+for word in word_candidates:
+    score_dist = phoneme_edit_distance('N OW N', word)
+    if score_dist < 0.5:
+        scores[word] = score_dist
+min = 100
+best = ""
+for k,v in scores.items():
+    if v < min:
+        best = k
+        min = v
+print(best, min)
 
 
-# 1 - misspelling - word
-mw_dict={'opend': 'opened', 'mabey': 'maybe', 'preffered': 'preferred', 'freindly': 'friendly', 'meat': 'meet',
-         'anoeing': 'annoying', 'detaled': 'detailed', 'respound': 'respond', 'preased': 'pressed', 'to': 'too',
-         'meny': 'many', 'vioce': 'voice', 'exiting': 'exciting', 'apeal': 'appeal', 'comfertable': 'confortable',
-         'detale': 'detail'}
-
-# 2 - misspelling - phonemes
-mph_dict={'opend': 'OW P AH N D', 'mabey': 'M EY B IY', 'preffered': 'P R EH F ER D', 'freindly': 'F R AY N D L IY',
-          'meat': 'M IY T', 'anoeing': 'AH N UW IH NG', 'detaled': 'D IH T EY L D', 'respound': 'R IY S P AW N D',
-          'preased': 'P R IY S T', 'to': 'T UW','meny': 'M EH N IY', 'vioce': 'V AY AH S',
-          'exiting': 'EH G Z AH T IH NG', 'apeal': 'EY P IY L', 'comfertable': 'K AH M F ER T AH B AH L',
-          'detale': 'D IH T EY L'}
-
-
-with open("C:/Users/robert/Documents/zeeko_nlp/input_files/phonemes_set.txt", 'rb') as cmu_phonemes:
-    cmu_phonemes = pickle.load(cmu_phonemes)
-    close_seq_of_phonemes_dict = {}
-
-    # results_dict len = 34013
-    for misspelling in list(mph_dict)[0:]:
-        close_seq_of_phonemes = []
-
-        # phonemes seq associated to the misspelling
-        phonemes = mph_dict.get(misspelling)
-        phonemes_list = phonemes.split(" ")
-
-        # make sure the list of close phonemes also contains the misspelled word's phonemes
-        # List of close sequence of phonemes?
-
-
-        # for each phoneme string sequence for mispelled word, add to list close_seq_phonemes
-        close_seq_of_phonemes.append(phonemes)
-
-        # for each unique phoneme sequence in the cmu dict
-        for i in cmu_phonemes:
-
-            # split sequence into individual phonemes
-            j = i.split(" ")
-
-            if j[:1] == phonemes_list[:1]:
-                if j[-1] == phonemes_list[-1]:
-                    dist = phoneme_edit_distance(phonemes_list, j)  # Emma's distance for phonemes
-                    try:
-                        if dist <= 0.5:
-                            close_seq_of_phonemes.append(i)
-                    except:
-                        print(j[-1])
-                        print( phonemes_list[-1])
-        close_seq_of_phonemes_dict[misspelling] = close_seq_of_phonemes
+stop = timeit.default_timer()
+print(scores)
+print('Time: ', stop - start)
