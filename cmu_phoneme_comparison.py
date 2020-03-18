a93491ea4f01a566f0b2117360d6f6160d9b65c8
Robert Young <robert.young.ie@gmail.com>
2020-03-18 15:58:38 +0000

    feat: Modularising code arpa and candidates
    Have created functions for generating the arpabet phoneme list, cmu
    valid phoneme sequence list and broken the Python spell check phoneme
    sequence generation functions up into one function.

--- cmu_phoneme_comparison.py
+++ cmu_phoneme_comparison.py
@@ -5,48 +5,56 @@ import timeit
 import re
 
 data_folder = Path("C:/Users/robert/Documents/zeeko_nlp/input_files/")
-similarities = data_folder / 'acoustic_similarity.csv'
+similarities = data_folder / 'acoustic_distributional_distance_matrix.csv'
 similarities = pd.read_csv(similarities, index_col=0)
 
 cmu_phonemes = data_folder / 'cmu_processed.csv'
 cmu_phonemes = pd.read_csv(cmu_phonemes, encoding = "ISO-8859-1")
 
-phonemes = []
-cmu_valid_phonemes = []
 
-for column in similarities.columns:
-    phonemes.append(column)
+def arpabet_phonemes():
+    phonemes = []
+    for column in similarities.columns:
+        phonemes.append(column)
+    valid_phonemes = (cmu_phonemes.iloc[:, 1])
+    valid_phonemes = valid_phonemes.tolist()
 
-valid_phonemes = (cmu_phonemes.iloc[:, 1])
-valid_phonemes = valid_phonemes.tolist()
+    return (phonemes, valid_phonemes)
+
 
 #  https://norvig.com/spell-correct.html
 
-def candidates(word):
-    "Generate possible spelling corrections for word."
-    return (known(edits1(word)) or known(edits2(word)))
+def generate_phoneme_candidates(word, phonemes, valid_phonemes):
 
-def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in valid_phonemes)
+    def candidates(word):
+        "Generate possible spelling corrections for word."
+        return known(edits1(word)) or known(edits2(word))
 
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters = phonemes
-    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes = [re.sub(' +', ' ',(L + " " + R[1:]).strip()) for L, R in splits if R]
-    transposes = [re.sub(' +', ' ',(L + " " + R[1] + " " + R[0] + " " + R[2:]).strip()) for L, R in splits if len(R)>1]
-    replaces = [re.sub(' +', ' ',(L + " " + c + " " + R[1:]).strip()) for L, R in splits if R for c in letters]
-    inserts = [re.sub(' +', ' ',(L + " " + c + " " + R).replace("  ", " ").strip()) for L, R in splits for c in letters]
-    return set([word] + deletes + transposes + replaces + inserts)
+    def known(words):
+        "The subset of `words` that appear in the dictionary of WORDS."
+        return set(w for w in words if w in valid_phonemes)
 
-def edits2(word):
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+    def edits1(word):
+        "All edits that are one edit away from `word`."
+        letters = phonemes
+        splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [re.sub(' +', ' ',(L + " " + R[1:]).strip()) for L, R in splits if R]
+        transposes = [re.sub(' +', ' ',(L + " " + R[1] + " " + R[0] + " " + R[2:]).strip()) for L, R in splits if len(R)>1]
+        replaces = [re.sub(' +', ' ',(L + " " + c + " " + R[1:]).strip()) for L, R in splits if R for c in letters]
+        inserts = [re.sub(' +', ' ',(L + " " + c + " " + R).replace("  ", " ").strip()) for L, R in splits for c in letters]
+        return set([word] + deletes + transposes + replaces + inserts)
+
+    def edits2(word):
+        "All edits that are two edits away from `word`."
+        return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+    return candidates(word)
+
+
 
 start = timeit.default_timer()
-
-word_candidates = (candidates('N OW N'))
+phonemes, valid_phonemes = arpabet_phonemes()
+word_candidates = (generate_phoneme_candidates('N OW N', phonemes, valid_phonemes))
 scores = {}
 
 for word in word_candidates:
@@ -55,6 +63,7 @@ for word in word_candidates:
         scores[word] = score_dist
 min = 100
 best = ""
+
 for k,v in scores.items():
     if v < min:
         best = k
