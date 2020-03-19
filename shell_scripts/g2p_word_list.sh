d01836762a8396be1e79d9a8cb4a33c24be2bbe7
Robert Young <robert.young.ie@gmail.com>
2020-03-19 09:47:05 +0000

    refactor: Updated folder location of word list file
    Word list file taken as input for the shell script to generate phoneme
    sequences as an output.

--- shell_scripts/g2p_word_list.sh
+++ shell_scripts/g2p_word_list.sh
@@ -2,8 +2,8 @@
 
 source ~/Documents/zeeko_nlp/nlp/Scripts/activate
 
-word_list_path="/c/Users/robert/Documents/zeeko_nlp/nlp/g2p-seq2seq-master/word_list.txt"
+word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/word_list.txt"
 model_path="/c/Users/robert/Documents/zeeko_nlp/nlp/g2p-seq2seq-master/g2p-seq2seq-model-6.2-cmudict-nostress"
-output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_output/results.txt"
+output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/results.txt"
 
 g2p-seq2seq --decode $word_list_path --model_dir $model_path --output "$output_path"
