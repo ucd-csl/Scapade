93235214de4e221aa046b3677150923aaf2c381e
Robert Young <robert.young.ie@gmail.com>
2020-03-27 12:34:28 +0000

    feat: Can accept holbrook or birkbeck on command line

--- shell_scripts/g2p_word_list.sh
+++ shell_scripts/g2p_word_list.sh
@@ -2,8 +2,14 @@
 
 source ~/Documents/zeeko_nlp/nlp/Scripts/activate
 
-word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/word_list.txt"
+str=$1
 model_path="/c/Users/robert/Documents/zeeko_nlp/nlp/g2p-seq2seq-master/g2p-seq2seq-model-6.2-cmudict-nostress"
-output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/results.txt"
 
+if [ "$str" == "birkbeck" ]; then
+  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/birkbeck_word_list.txt"
+  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/birkbeck_phonemes.txt"
+elif [ "$str" == "holbrook" ]; then
+  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/holbrook_word_list.txt"
+  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/holbrook_phonemes.txt"
+fi
 g2p-seq2seq --decode $word_list_path --model_dir $model_path --output "$output_path"
