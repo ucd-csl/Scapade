2aa5ae3918388435db5b9b0f1db3c5e6fd78487e
Robert Young <robert.young.ie@gmail.com>
2020-04-08 10:16:42 +0100

    refactor: Updated paths from abs to rel, removed source

--- shell_scripts/g2p_word_list.sh
+++ shell_scripts/g2p_word_list.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env sh
 
-source ~/Documents/zeeko_nlp/nlp/Scripts/activate
-
 str=$1
 model_path="/c/Users/robert/Documents/zeeko_nlp/nlp/g2p-seq2seq-master/g2p-seq2seq-model-6.2-cmudict-nostress"
 
 if [ "$str" == "birkbeck" ]; then
-  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/birkbeck_word_list.txt"
-  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/birkbeck_phonemes.txt"
+  word_list_path="../g2p_files/birkbeck_word_list.txt"
+  output_path="../g2p_files/birkbeck_phonemes.txt"
 elif [ "$str" == "holbrook" ]; then
-  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/holbrook_word_list.txt"
-  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/holbrook_phonemes.txt"
+  word_list_path="../g2p_files/holbrook_word_list.txt"
+  output_path="../g2p_files/holbrook_phonemes.txt"
 elif [  "$str" == "zeeko" ]; then
-  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/zeeko_word_list.txt"
-  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/zeeko_phonemes.txt"
+  word_list_path="../g2p_files/zeeko_word_list.txt"
+  output_path="../g2p_files/zeeko_phonemes.txt"
 elif [ "$str" == "aspell" ]; then
-  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/aspell_word_list.txt"
-  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/aspell_phonemes.txt"
+  word_list_path="../g2p_files/aspell_word_list.txt"
+  output_path="../g2p_files/aspell_phonemes.txt"
 elif [ "$str" == "wiki" ]; then
-  word_list_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/wiki_word_list.txt"
-  output_path="/c/Users/robert/Documents/zeeko_nlp/g2p_files/wiki_phonemes.txt"
+  word_list_path="../g2p_files/wiki_word_list.txt"
+  output_path="../g2p_files/wiki_phonemes.txt"
 fi
 
 g2p-seq2seq --decode $word_list_path --model_dir $model_path --output "$output_path"
