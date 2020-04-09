5537c8eb8483e86edbc89fb5431a83453d683428
Robert Young <robert.young.ie@gmail.com>
2020-04-09 17:08:46 +0100

    feat: First commit of step to put cmu results in csv

---
+++ data_analysis/create_results_csv.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pickle
+
+complete_corrections_path = "../input_files/spelling_correction_dicts/"
+output_path = "../data_analysis/results/"
+dataset_names = ['holbrook', 'birkbeck', 'zeeko', 'aspell', 'wiki']
+complete_results = {}
+output_paths = {}
+
+for dataset in dataset_names:
+    complete_results[dataset] = complete_corrections_path + dataset + '_phonemes_sym.txt'
+    output_paths[dataset] = output_path + dataset + "/symspell_cmu_phonemes/" + "all_cmu_corrections.csv"
+
+
+def load_results(file_path):
+    results = pickle.load(open(file_path, "rb"))
+    return results
+
+
+def update_results_dict(input_dict):
+    for key in list(input_dict):
+        input_dict[key]['misspelling'] = key
+        del input_dict[key]['candidates']
+        if input_dict[key]['correct_spelling'] == input_dict[key]['suggested']:
+            input_dict[key]['correct'] = 1
+        else:
+            input_dict[key]['correct'] = 0
+    return input_dict
+
+
+def create_df_output(input_dict):
+    output = pd.DataFrame()
+    for key, values in input_dict.items():
+        output = output.append(input_dict[key], ignore_index=True)
+    return output
+
+def save_to_csv():
+    pass
