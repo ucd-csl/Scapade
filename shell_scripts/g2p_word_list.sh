8c24baa814779afd1278f955796f4c0f2e1a6cb7
Robert Young <robert.young.ie@gmail.com>
2020-04-10 10:16:43 +0100

    refactor: Simplified code, allowed custom word lists

--- shell_scripts/g2p_word_list.sh
+++ shell_scripts/g2p_word_list.sh
@@ -4,21 +4,8 @@ str=$1
 # edit this path to where the model rests in your file structure
 model_path="/c/Users/robert/Documents/zeeko_nlp/nlp/g2p-seq2seq-master/g2p-seq2seq-model-6.2-cmudict-nostress"
 
-if [ "$str" == "birkbeck" ]; then
-  word_list_path="../g2p_files/birkbeck_word_list.txt"
-  output_path="../g2p_files/birkbeck_phonemes.txt"
-elif [ "$str" == "holbrook" ]; then
-  word_list_path="../g2p_files/holbrook_word_list.txt"
-  output_path="../g2p_files/holbrook_phonemes.txt"
-elif [  "$str" == "zeeko" ]; then
-  word_list_path="../g2p_files/zeeko_word_list.txt"
-  output_path="../g2p_files/zeeko_phonemes.txt"
-elif [ "$str" == "aspell" ]; then
-  word_list_path="../g2p_files/aspell_word_list.txt"
-  output_path="../g2p_files/aspell_phonemes.txt"
-elif [ "$str" == "wiki" ]; then
-  word_list_path="../g2p_files/wiki_word_list.txt"
-  output_path="../g2p_files/wiki_phonemes.txt"
-fi
+g2p_path="../g2p_files/"
+word_list_path="${g2p_path}${str}_word_list.txt"
+output_path="${g2p_path}${str}_phonemes.txt"
 
 g2p-seq2seq --decode $word_list_path --model_dir $model_path --output "$output_path"
