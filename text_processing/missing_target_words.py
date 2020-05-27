41908d940f347f51ba256ea5d10c9f88714b1155
Robert <robert.young.ie@gmail.com>
2020-05-27 16:00:48 +0100

    feat: Modularised all operations

--- text_processing/missing_target_words.py
+++ text_processing/missing_target_words.py
@@ -1,4 +1,8 @@
 import pickle
+import os.path
+import pandas as pd
+from spellchecker import SpellChecker
+import aspell
 
 # load in misspelling datasets
 file_path = '../input_files/'
@@ -7,17 +11,62 @@ birkbeck = file_path + 'birkbeck.txt'
 holbrook = file_path + 'holbrook-missp.txt'
 wikipedia = file_path + 'wikipedia.txt'
 zeeko = file_path + 'zeeko_misspellings.txt'
-
+output_file = '../spelling_mistakes/' + 'target_words_all.txt'
 datasets = {'aspell': aspell, 'birkbeck': birkbeck, 'holbrook': holbrook, 'wikipedia': wikipedia, 'zeeko': zeeko}
 
-# get all the target misspellings for each dataset and store these as lower case words in a set
-all_set = set()
+# load in dictionaries for each tool
 
-for key, value in datasets.items():
-    with open(datasets[key], 'r') as f:
+cmu_dict = '../symspellpy_phonemes/cmu_frequency.csv'
+symspell_words = '../symspellpy_words/frequency_dictionary_en_82_765.txt'
+target_words = list(pickle.load(open(output_file, 'rb')))
+
+
+def get_targets():
+    # get all the target misspellings for each dataset and store these as lower case words in a set
+    if not os.path.exists(output_file):
+        all_set = set()
+        for key, value in datasets.items():
+            with open(datasets[key], 'r') as f:
+                lines = f.read().splitlines()
+                for line in lines:
+                    if line[0] == '$' and '_' not in line:
+                        all_set.add(line[1:].lower())
+
+        pickle.dump(all_set, open(output_file, 'wb'))
+
+
+def missing_targets_cmu(cmu_dict, target_words):
+    df_cmu = pd.read_csv(cmu_dict)
+    all_words = (df_cmu.iloc[:, 0].tolist())
+    all_words = set(all_words)
+    missing_words = set()
+    for word in target_words:
+        if word not in all_words:
+            missing_words.add(word)
+
+
+def missing_targets_sym(symspell_dict, target_words):
+    missing_words = set()
+    with open(symspell_dict, 'r') as f:
         lines = f.read().splitlines()
         for line in lines:
-            if line[0] == '$' and '_' not in line:
-                all_set.add(line[1:].lower())
+            current_word = line.split()[0]
+            if current_word not in target_words:
+                missing_words.add(current_word)
 
-print(len(all_set))
+
+def missing_targets_aspell(target_words):
+    s = aspell.Speller('lang', 'en')
+    missing_words = set()
+    for word in target_words:
+        if s.check(word) == False:
+            missing_words.add(word)
+
+
+def missing_targets_pyspell(target_words):
+    spell = SpellChecker()
+    misspelled = spell.unknown(target_words)
+
+
+
+
